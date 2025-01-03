c4d46dcd05e1503d070d5135f715001aa5ba135f
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2025-01-03 18:05:04 +0100

    init repo

---
+++ src/main/java/it/unisa/greenbottle/GreenBottleApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.greenbottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBottleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreenBottleApplication.class, args);
+  }
+
+}
