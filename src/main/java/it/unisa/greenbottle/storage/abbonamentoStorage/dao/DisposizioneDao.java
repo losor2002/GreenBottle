9290996ca4316398cb14e3dde2e12322e225cd83
GiuseppePastena23 <giuseppe.pastena23@gmail.com>
2025-01-16 17:00:34 +0100

    Update DisposizioneDao.java
    updated sql query

--- src/main/java/it/unisa/greenbottle/storage/abbonamentoStorage/dao/DisposizioneDao.java
+++ src/main/java/it/unisa/greenbottle/storage/abbonamentoStorage/dao/DisposizioneDao.java
@@ -3,6 +3,7 @@ package it.unisa.greenbottle.storage.abbonamentoStorage.dao;
 import it.unisa.greenbottle.storage.abbonamentoStorage.entity.Abbonamento;
 import it.unisa.greenbottle.storage.abbonamentoStorage.entity.Disposizione;
 import it.unisa.greenbottle.storage.catalogoStorage.entity.Prodotto;
+import org.hibernate.annotations.processing.SQL;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +12,7 @@ import java.util.Map;
 public interface DisposizioneDao extends JpaRepository<Disposizione, Long> {
 
     List<Prodotto> findByAbbonamento(Abbonamento abbonamento);
+
+    @SQL("select prodotto, quantita from Disposizione join Abbonamento on Disposizione.id= Abbonamento.id where Abbonamento.tipo = :1?")
     Map<Prodotto, Integer> findByAllByAbbonamentoAndTipo(Abbonamento abbonamento, Abbonamento.TipoAbbonamento tipoAbbonamento);
 }
